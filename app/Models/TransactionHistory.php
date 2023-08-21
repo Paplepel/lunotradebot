da3a844568ad9d2ad4c99031f5ff8ecd0fd1f0d2
Adriaan van Niekerk <adriaan@fgx.co.za>
2023-08-21 10:47:16 +0200

    Added composer web file

---
+++ app/Models/TransactionHistory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionHistory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transaction_history';
+
+    protected $fillable = [
+        'pair',
+        'type',
+        'amount',
+        'price',
+        'stop_loss',
+    ];
+}
