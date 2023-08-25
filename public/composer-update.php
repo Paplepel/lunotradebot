f7f8c2c1b22636c14a80ad1acfbdd42e8288d249
Adriaan van Niekerk <adriaan@fgx.co.za>
2023-08-25 10:15:41 +0200

    Added composer web file

--- public/composer-update.php
+++ public/composer-update.php
@@ -1,3 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 echo shell_exec('/usr/local/bin/ea-php81 /home/sopleudy/public_html/composer.phar update');
 ?>
